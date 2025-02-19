e30f65d8a1493b45defe4dc777a972e7ac20db12
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-19 19:23:29 +0530

    update

--- Scope.java
+++ Scope.java
@@ -10,19 +10,19 @@ public class Scope
     public void testFunc(int x) {
         // Method Scope (t)
         Scope t = new Scope(); 
-        this.x = 22; 
+        this.x = 22; //differentiating the member variable with parameter(local) variable //once static variable is changed it will effevt for all objects
         y = 44; 
 
         // Printing variables with different scopes
         System.out.println("Scope.x: " + Scope.x); 
         System.out.println("t.x: " + t.x); 
-        System.out.println("t.y: " + t.y); 
-        System.out.println("y: " + y); 
+        System.out.println("t.y: " + t.y); //private variable can be accessed by public method
+        System.out.println("y: " + y); //local y value
     }
 
     // Main Method
     public static void main(String args[]) {
         Scope t = new Scope();
-        t.testFunc(5); 
+        t.testFunc(5); //calling non static function
     }
 }
